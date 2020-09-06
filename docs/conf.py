746c216eea6b788c74b36c8d9c91bfda7eaaa135
amjadraza <amjadraza24@gmail.com>
2020-09-06 12:01:54 +1000

    change the copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'retail-sales-prediction'
-copyright = u"2019, MA RAZA"
+copyright = u"2020, MA RAZA"
 author = u"MA RAZA"
 
 # The version info for the project you're documenting, acts as replacement
